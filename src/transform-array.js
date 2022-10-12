c7c9eba2634b3c638427d4f5e4daa3b53caf7275
Pavel Didenko <win4ester2016@yandex.ru>
2022-10-12 21:43:48 +0300

    feat: implement transform array function

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,61 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function transform(arr) {
+  const control_sequences = ['--double-prev', '--double-next', '--discard-prev', '--discard-next']
+  let transformed = [];
+
+  if (arr instanceof Array !== true){
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }else if(arr == []){
+    return '[]';
+  }
+
+  for (let a of arr){
+    transformed.push(a)
+  }
+
+  for(let i = 0; i < transformed.length; i++){
+    if(transformed[i] == control_sequences[0]){
+      if(i > 0){
+        transformed[i] = transformed[i - 1];
+        transformed =  transform(transformed)
+      }else{
+        transformed.shift(transformed[i])
+        transformed =  transform(transformed)
+      }
+      
+    }else if(transformed[i] == control_sequences[1]){
+      if(i < transformed.length - 1){
+        transformed[i] = transformed[i + 1];
+        transformed =  transform(transformed)
+      }else{
+        transformed.pop(transformed[i])
+        transformed =  transform(transformed)
+      }
+    }else if (transformed[i] == control_sequences[2] && i-1 >= 0){
+      transformed.splice(i-1, 2);
+      transformed =  transform(transformed)
+    }else if(transformed[i] == control_sequences[2] && i==0){
+      transformed.unshift(i);
+      transformed =  transform(transformed)
+      
+    }
+    else if (transformed[i] == control_sequences[3]){  //'--discard-next'
+      if(i < transformed.length - 1){
+        transformed.splice(i, 2);
+        if(transformed[i] == control_sequences[0] || control_sequences[2]){
+          transformed.splice(i, 1)
+          transformed =  transform(transformed)
+        } 
+      }else{
+        transformed.pop(transformed[i])
+        transformed =  transform(transformed)
+      }
+      
+    }
+  }
+  return transformed;
 }
 
 module.exports = {
