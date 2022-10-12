c08abc68a3f6aa32d597c224da301375d83d8900
Pavel Didenko <win4ester2016@yandex.ru>
2022-10-12 10:51:24 +0300

    fix: invalid dates handler

--- src/what-season.js
+++ src/what-season.js
@@ -11,19 +11,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(date) {
+ function getSeason(date) {
   const seasons = ['winter', 'spring', 'summer', 'autumn'];
 
-  if(typeof date.getMonth != 'function'){
-    return "Unable to determine the time of year";
-  }else if(date.getMonth > 3){
+  if(date == undefined){
+    return 'Unable to determine the time of year!';
+  }
+
+  if(date instanceof(Date) != true || date == Date()){
+    // let err = new Error("Invalid date!");
+    throw new Error("Invalid date!")
+    }
+
+
+  if(date.getMonth() <= 1 || date.getMonth() == 11){
     return seasons[0];
-  }else if(date.getMonth < 6){
+  }else if(date.getMonth() <= 4){ //Spring
     return seasons[1];
-  }else if(date.getMonth > 9){
+  }else if(date.getMonth() <= 7){ //Summer
+    return seasons[2];
+  }else if (date.getMonth() <= 10){ //Autumn
     return seasons[3];
-  }else if (date.getDate < 12){
-    return seasons[4];
   }
 }
 
