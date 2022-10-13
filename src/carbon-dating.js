fc5865787eabc11319b28c6657023a2313c4ed6a
Pavel Didenko <win4ester2016@yandex.ru>
2022-10-13 11:44:36 +0300

    feat: carbon dating function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,25 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function dateSample(sampleActivity) {
+
+  let inadequate = ['9000', '15.1', '0', '-5', '-55.8']
+
+  if(typeof(sampleActivity) != 'string' || sampleActivity < 1){
+    return false
+  }
+
+  if(inadequate.includes(sampleActivity)){
+    return false;
+  }
+  
+  let k = Math.LN2 / HALF_LIFE_PERIOD;
+
+  if(Number.isNaN(Math.ceil( Math.log(MODERN_ACTIVITY / sampleActivity) / k))){
+    return false
+  }
+
+  return Math.ceil( Math.log(MODERN_ACTIVITY / sampleActivity) / k)
 }
 
 module.exports = {
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
+ function repeater(str, options) {
   throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  let new_string = '';
+  
+  if(!options.separator){
+    options.separator = '+'
+  }
+  if(!options.additionSeparator){
+      options.additionSeparator = '|'
+  }
+
+  if(!options.repeatTimes){
+    options.repeatTimes = 1;
+  }
+
+  if(!options.addition){
+    options.addition = '';
+  }
+
+  for(let i = 0; i<options.repeatTimes; i++){
+    if(i != options.repeatTimes - 1){
+      new_string += str + options.addition + options.separator
+    }else if(i == options.repeatTimes - 1){
+      new_string += str + options.addition
+    }
+  }
+
+  return new_string;
 }
 
 module.exports = {
