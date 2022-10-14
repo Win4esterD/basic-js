fce34a38184bad7b6bca9ce821fc00136accf74c
Pavel Didenko <win4ester2016@yandex.ru>
2022-10-14 03:21:54 +0300

    feat: add getMaxofArray function

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function deleteDigit(n) {
+  let str_num = n.toString();
+  let result_arr = [];
+  let temp_arr = str_num.split('');
+
+  for(let i = 0; i < str_num.length; i++){
+    result_arr.push(Number(str_num.slice(0, i) + (str_num.slice(i+1))))
+  }
+
+  function getMaxOfArray(numArray) {
+    return Math.max.apply(null, numArray);
+  }
+
+
+  return getMaxOfArray(result_arr);
+  
 }
 
 module.exports = {
